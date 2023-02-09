95f814a916b746cd7686ed8f7e34581f65d22255
mujahedu <124909688+mujahedu@users.noreply.github.com>
2023-02-09 23:11:10 +0530

    Add files via upload

---
+++ DAY3_b74(1).py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+continuation with string data type:
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+first_name = 'bharat'
+last_name = 'reddy'
+
+
+# In[8]:
+
+
+# req: i want to get the full name ?
+
+
+# In[ ]:
+
+
+
+
+
+# In[9]:
+
+
+understanding the concept of f tring **
+
+
+# In[10]:
+
+
+# general syntax of f strings
+
+
+# In[11]:
+
+
+f" custom words {place_holder - 1} {placeholder-2 }........{place_holder ----n }"
+
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+full_name = f"{first_name} {last_name}"
+
+print(full_name)
+
+
+# In[13]:
+
+
+message = f"keep up the good work, {full_name.title()}"
+
+print(message)
+
+
+# In[ ]:
+
+
+
+
+
+# In[14]:
+
+
+adding the whitespaces :
+
+
+# In[15]:
+
+
+print("favourite_programming_language:pythonjavac++swiftpascalcobal")
+
+
+# In[16]:
+
+
+\n -----> it is called as new line delinter
+
+
+# In[17]:
+
+
+print("favourite_programming_language_:\npython\njava\nc++\nswift\npascal\ncobal")
+
+
+# In[18]:
+
+
+\t ====> tab delinter
+
+
+# In[20]:
+
+
+print("favourite programming language:\n\tpython\n\tjava\n\tc++\n\tswift\n\tpascal\n\tcobal")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+removing whitespaces from strings :
+
+
+# In[27]:
+
+
+name = 'python'
+print(name)
+
+
+# In[28]:
+
+
+name2 = '    python'
+print(name2)
+
+
+# In[30]:
+
+
+name3 = 'python      '
+print(name3)
+
+
+# In[35]:
+
+
+name2.lstrip()
+
+
+# In[36]:
+
+
+name3.rstrip()
+
+
+# In[37]:
+
+
+name4 = '    python    '
+print(name4)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
